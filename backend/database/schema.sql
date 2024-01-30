e1fd010a3a004d2a7de3fa2b637032d187c3c474
Christopher Arthaud <christopher.arthaud@gmail.com>
2024-01-30 09:43:09 +0100

    BDD created and seeded

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -16,14 +16,14 @@ CREATE TABLE IF NOT EXISTS `customer` (
 DROP TABLE IF EXISTS `category` ;
 
 CREATE TABLE IF NOT EXISTS `category` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id`));
 
 DROP TABLE IF EXISTS `professional` ;
 
 CREATE TABLE IF NOT EXISTS `professional` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
   `activity` VARCHAR(100) NOT NULL,
   `description` VARCHAR(500) NOT NULL,
@@ -35,19 +35,17 @@ CREATE TABLE IF NOT EXISTS `professional` (
 DROP TABLE IF EXISTS `service` ;
 
 CREATE TABLE IF NOT EXISTS `service` (
-  `id` INT NOT NULL,
-  `duration` INT NOT NULL DEFAULT,
-  `price` INT NOT NULL DEFAULT,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `duration` INT NOT NULL,
+  `price` INT NOT NULL,
   `name` VARCHAR(100) NOT NULL,
   `id_category` INT NOT NULL,
   `id_professional` INT NOT NULL,
   PRIMARY KEY (`id`),
 
-  CONSTRAINT `id_category`
     FOREIGN KEY (`id_category`)
-    REFERENCES `category`(`id`)
+    REFERENCES `category`(`id`),
 
-  CONSTRAINT `id_professional`
     FOREIGN KEY (`id_professional`)
     REFERENCES `professional`(`id`));
 
@@ -59,13 +57,11 @@ CREATE TABLE IF NOT EXISTS `customer_service` (
   `service_id` INT NOT NULL,
   `date_beginning` DATETIME NULL,
   `date_end` DATETIME NULL,
-  PRIMARY KEY (`customer_id`, `service_id`),
+  PRIMARY KEY (`customer_id`, `date_beginning`),
 
-  CONSTRAINT `customer_id`
     FOREIGN KEY (`customer_id`)
-    REFERENCES `customer`(`id`)
+    REFERENCES `customer`(`id`),
 
-  CONSTRAINT `service_id`
     FOREIGN KEY (`service_id`)
     REFERENCES `service`(`id`));
 
@@ -73,11 +69,10 @@ CREATE TABLE IF NOT EXISTS `customer_service` (
 DROP TABLE IF EXISTS `image` ;
 
 CREATE TABLE IF NOT EXISTS `image` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `link` VARCHAR(100) NOT NULL,
   `id_professional` INT NOT NULL,
   PRIMARY KEY (`id`),
 
-  CONSTRAINT `id_professional`
     FOREIGN KEY (`id_professional`)
     REFERENCES `professional`(`id`));
